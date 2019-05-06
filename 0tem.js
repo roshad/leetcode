707d5d2c7bcc00618dfa21f4703cefd492702144
roshad <roshad87a9@gmail.com>
2019-05-06 19:03:06 +0800

    73

--- 0tem.js
+++ 0tem.js
@@ -1,12 +1,20 @@
-function leastInterval(tasks, n) {
-    let fre = Array(26).fill(0)
+function setZeroes(matrix) {
+    const 
+        clen = matrix.length,
+        rlen = matrix[0].length
+    let c0 = false
 
-    for (let task of tasks) fre[task.charCodeAt(0) - "A".charCodeAt(0)]++
+    for (let yi = 0; yi < clen.length; yi++) {
+        if (matrix[yi][0]) c0=true
+        for (let xi = 1; xi < rlen.length; xi++) {            
+            if (matrix[yi][xi]==0) matrix[yi][0]=matrix[0][xi]=0
+        }        
+    }
 
-    fre.sort((a, b) => b - a)
-
-    let fs = 1 //frequency same 
-    while (fs < 26 && fre[fs] == fre[0]) fs++
-
-    return Math.max(tasks.length, (fre[0] - 1) * (n + 1) + fs)
+    for (let yi = clen.length-1; yi >-1; yi--) {
+        for (let xi = rlen.length-1; xi >0; xi--) {
+            if (matrix[yi][0]==0||matrix[0][xi]==0)matrix[yi][xi]=0
+        }
+        if (matrix[yi][c0]==true)matrix[yi][0]=0
+    }
 }
--- 73. Set Matrix Zeroes.js
+++ 73. Set Matrix Zeroes.js
@@ -1,23 +1,22 @@
 function setZeroes(matrix) {
-    let r0 = false
-    let rLen = matrix[0].length;
-    let cLen = matrix.length;
+    const
+        clen = matrix.length,
+        rlen = matrix[0].length
+    let c0 = false
 
-    for (let xi = 0; xi < rLen; xi++) {
-        if (matrix[0][xi] == 0) r0 = true
-        for (let yi = 1; yi < cLen; yi++) {
-            if (matrix[yi][xi] == 0) {
-                matrix[yi][0] = matrix[0][xi] = 0;
-            }
-        }
+    for (let yi = 0; yi < clen; yi++) {
+        if (matrix[yi][0] == 0) c0 = true
+        for (let xi = 1; xi < rlen; xi++)
+            if (matrix[yi][xi] == 0)
+                matrix[yi][0] = matrix[0][xi] = 0
+
     }
 
-    for (let xi = rLen - 1; xi > -1; xi--) {
-        for (let yi = cLen - 1; yi > 0; yi--) {
-            if (matrix[0][xi] == 0 || matrix[yi][0] == 0) {
+    for (let yi = clen - 1; yi > -1; yi--) {
+        for (let xi = rlen - 1; xi > 0; xi--)
+            if (matrix[yi][0] == 0 || matrix[0][xi] == 0)
                 matrix[yi][xi] = 0
-            }
-        }
-        if (r0 == true) matrix[0][xi] = 0
+
+        if (c0 == true) matrix[yi][0] = 0
     }
 }
