d3d6df5645c4cf2076584e7d57ebc3d9ffec32b7
roshad <roshad87a9@gmail.com>
2019-05-16 12:35:55 +0800

    189

--- 189. Rotate Array.js
+++ 189. Rotate Array.js
@@ -1,36 +1,34 @@
-
-var rotate = function (nums, k) {
-    k %= nums.length
-    let count = 0;
-    for (let start_I = 0; count < nums.length; start_I++) {
-        let cur_I = start_I;
-        let pre_num = nums[start_I];
-        
+function rotate(nums, k){
+    let 
+        i_sta = -1,
+        len = nums.length,
+        count=0
+    k %= len
+    while (count!=len){
+        i_sta++
+        let 
+            i_cur=i_sta,
+            holder= nums[i_cur]
         do {
-            let next = (cur_I + k) % nums.length;
-            let temp = nums[next];
-            nums[next] = pre_num;
-            pre_num = temp;
-            cur_I = next;
-            count++;
-        } while (start_I != cur_I);
-    }
-    
-};
-//64ms
+            let i_nex = (i_cur+k)%len;            
+            [holder,nums[i_nex]] = [nums[i_nex],holder]
+            i_cur=i_nex
+            count++
+        } while (i_cur!=i_sta)
+    } 
+}
+
 function rotate(nums, k){
     k %= nums.length
     reverse(0, nums.length - 1)
     reverse(0,k-1)
     reverse(k,nums.length - 1)
 
-    function reverse(sta,end){
-        let tem
-        while (sta<end){
-            tem = nums[sta]
-            nums[sta] = nums[end]
-            nums[end] = tem
+    function reverse(sta,end){        
+        while (sta<end){            
+            [nums[sta],nums[end]] = [nums[end],nums[sta]]            
             sta++;end--
         }
     }
 }
+
