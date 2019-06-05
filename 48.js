1b7193b46783d03e91b7cfd4b8fe82232645aad7
roshad <roshad87a9@gmail.com>
2019-06-06 01:16:05 +0800

    48

---
+++ 48.js
@@ -0,0 +1,12 @@
+var rotate = function (matrix) {
+    let len = matrix.length, max_ind = len - 1
+
+    for (let y_ind = 0; y_ind < Math.floor(len / 2); y_ind++)
+        for (let x_ind = y_ind; x_ind < max_ind - y_ind; x_ind++) {
+            let tem = matrix[y_ind][x_ind]
+            matrix[y_ind][x_ind] = matrix[max_ind - x_ind][y_ind]
+            matrix[max_ind - x_ind][y_ind] = matrix[max_ind - y_ind][max_ind - x_ind]
+            matrix[max_ind - y_ind][max_ind - x_ind] = matrix[x_ind][max_ind - y_ind]
+            matrix[x_ind][max_ind - y_ind] = tem
+        }
+}
