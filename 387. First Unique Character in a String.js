6532a5a0aed7f3a3d5f7b13a901a4ff3ec63a42f
roshad <roshad87a9@gmail.com>
2019-06-08 20:21:44 +0800

    387

--- 387. First Unique Character in a String.js
+++ 387. First Unique Character in a String.js
@@ -1,37 +1,22 @@
-var firstUniqChar = function(s) {
-    mem = {}
-    for (let i in s) {
-        let cha = s[i]
-        if (!mem[cha]) {
-            mem[cha]={}
-            mem[cha]["first"] = i*1
-        }
-        mem[cha]["amount"]= mem[cha]["amount"]+1 || 1
+//1
+var firstUniqChar = function (s) {
+    const aa = {}
+
+    for (let cha of s) {
+
+        aa[cha] = aa[cha] + 1 || 1
     }
-    //console.log(mem)
-    
-    for (let cha in mem) {
-        //console.log(k,v)
-        if (mem[cha]["amount"]==1) return mem[cha]["first"]
-        
+
+    for (let cha in aa) {
+        if (aa[cha] == 1) return s.indexOf(cha)
     }
     return -1
-};
-
-
+}
+//2
 var firstUniqChar = function (s) {
-    const letters = 'abcdefghijklmnopqrstuvwxyz'
-    let firstUniq = s.length
-
-    for (let i = 0; i < letters.length; i++) {
-        const char = letters[i],
-            first = s.indexOf(char)
-        if (
-            first !== -1 &&//exist 
-            s.lastIndexOf(char) === first && //unique
-            firstUniq > first // first
-        ) { firstUniq = first }
+    for (let cha of s){
+        const fInd = s.indexOf(cha) 
+        if (fInd== s.lastIndexOf(cha)) return fInd
     }
-    
-    return firstUniq === s.length ? -1 : firstUniq
-};
+    return -1;
+}
