7743eb8116113a540be1102e36e6f3e5ed41ad17
roshad <roshad87a9@gmail.com>
2019-06-09 17:44:49 +0800

    242

--- 242. Valid Anagram.js
+++ 242. Valid Anagram.js
@@ -1,15 +1,19 @@
-var isAnagram = function(s, t) {
+//1
+var isAnagram = function (s, t) {
     if (s.length != t.length) return false
+    let aa = {}, aa1 = {}
+    for (let cha of s) aa[cha] = aa[cha] + 1 || 1
+    for (let cha of t) aa1[cha] = aa1[cha] + 1 || 1
 
-    let mem={}
-    for (let cha of s) mem[cha] = mem[cha]+1 ||1
-    //console.log(mem)
-    for (let cha of t){
-        mem[cha] -= 1
-        //console.log(cha,mem)
-        if (!(mem[cha]>-1)) return false
-    }
+    for (let cha in aa) if (aa1[cha] !== aa[cha]) return false
 
     return true
+}
+//2
+var isAnagram = function (s, t) {
+    if (s.length != t.length) return false
+    let aa = {}
+    for (let cha of s) aa[cha] = aa[cha] + 1 || 1
+    for (let cha of t) if (!(--aa[cha] > -1)) return false
+    return true
 };
-
