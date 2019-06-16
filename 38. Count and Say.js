f387d9915c3c04bdf1bb9e5377636f6eb7f0bc3e
roshad <roshad87a9@gmail.com>
2019-06-16 11:40:12 +0800

    38

--- 38. Count and Say.js
+++ 38. Count and Say.js
@@ -1,18 +1,17 @@
-var countAndSay = function(n) {
-    let num = "1",digit;
+var countAndSay = function (n) {
+    let cur_num = '1'
+    while (n > 1) {
+        let next_num = ''
+        for (let ind = 0; ind < cur_num.length;) {
+            let cur_digit = cur_num[ind], cur_digit_amount = 1
 
-    while (n>1){
-        let tem = ""
-        for (let ind =0,amount=0;ind<num.length;ind++){
-            digit = num[ind]
-            while (num[ind]==digit) {
-                ind++;amount++
-            }
-            console.log(digit,amount)
-            tem += amount + digit
+            while (cur_num[ind] === cur_num[++ind]) { cur_digit_amount++ }
+
+            next_num += cur_digit_amount + "" + cur_digit
+
         }
-        num = tem
+        cur_num = next_num
         n--
     }
-    return num
-};
+    return cur_num
+}
