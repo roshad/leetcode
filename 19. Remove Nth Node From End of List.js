bc23b519f71f695a9358e534c3a4ace43235ffdf
roshad <roshad87a9@gmail.com>
2019-06-20 08:05:13 +0800

    19

--- 19. Remove Nth Node From End of List.js
+++ 19. Remove Nth Node From End of List.js
@@ -1,18 +1,34 @@
+//1
 var removeNthFromEnd = function (head, n) {
     let reader = head, len = 0
     while (reader) {
         len++
         reader = reader.next
     }
-    //console.log(len)
-
-    let dummy = new ListNode()
-    dummy.next = head
-    reader = dummy
+    
+    let mem_head = new ListNode()
+    mem_head.next = head
+    reader = mem_head
 
     let steps = len - n
     while (steps--) reader = reader.next
 
     reader.next = reader.next.next
-    return dummy.next
+    return mem_head.next
 };
+//2 
+var removeNthFromEnd = function (head, n) {    
+    let p_r=head
+    while (n){
+        p_r=p_r.next
+        n--
+    }
+    if (!p_r) return head.next
+    let p_l=head
+    while (p_r.next){
+        p_l=p_l.next
+        p_r=p_r.next
+    }
+    p_l.next=p_l.next.next
+    return head
+}
