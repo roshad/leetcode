8b66ccfc2879a9e6e1ef495f6a7216f250a092f2
roshad <roshad87a9@gmail.com>
2019-06-22 20:16:29 +0800

    206

--- 206. Reverse Linked List.js
+++ 206. Reverse Linked List.js
@@ -9,3 +9,14 @@ var reverseList = function(head) {
     }
     return cur
 };
+
+var reverseList = function(head) { 
+
+    const recurse= (lef,mid,rig)=>{
+        if (!mid) return lef
+        mid.next=lef
+        lef=mid;mid=rig;rig=rig||{}.next
+        return recurse(lef,mid,rig)
+    }
+    return recurse(head,head.next,head.next.next)
+}
