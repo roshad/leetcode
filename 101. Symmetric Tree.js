9774a7e59587024d0970676410e69ef27ec2471c
roshad <roshad87a9@gmail.com>
2019-07-11 09:18:43 +0800

    101

--- 101. Symmetric Tree.js
+++ 101. Symmetric Tree.js
@@ -1,11 +1,28 @@
-var isSymmetric = function(nodeL,nodeR=nodeL) {
-    
-    if (!nodeL&&!nodeR) return true
-    if (!nodeL||!nodeR) return false
-    
+var isSymmetric = function(Lnode, Rnode = Lnode) {
+    if (Lnode == null && Rnode == null) return true;
+
+    if (Lnode == null || Rnode == null) return false;
     return (
-        nodeL.val == nodeR.val &&
-        isSymmetric(nodeL.right,nodeR.left)&&
-        isSymmetric(nodeL.left,nodeR.right)
-    )
+        Lnode.val == Rnode.val &&
+        isSymmetric(Lnode.left, Rnode.right) &&
+        isSymmetric(Lnode.right, Rnode.left)
+    );
+};
+
+var isSymmetric = function(Lnode, Rnode = Lnode) {
+    let que = [];
+    que.unshift(Lnode, Rnode);
+
+    while (que.length) {        
+        let n1 = que.shift(),
+            n2 = que.shift();
+        if (n1 == null && n2 == null) continue;
+        if (n1 == null || n2 == null) return false;
+        if (!(n1.val == n2.val)) return false;
+        que.unshift(n1.left);
+        que.unshift(n2.right);
+        que.unshift(n1.right);
+        que.unshift(n2.left);
+    }
+    return true
 };
