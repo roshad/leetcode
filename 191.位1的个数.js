76cdf5d4e4f55f4fbc5390748cc8cd82d3da7428
roshad <roshad87a9@gmail.com>
2019-08-18 17:25:49 +0800

    191

---
+++ 191.位1的个数.js
@@ -0,0 +1,19 @@
+var hammingWeight = function(n) {
+    let ret =0
+    
+    while(n){        
+        if (n%2) ret++
+        n=Math.floor(n/2)
+    }
+    return ret
+};
+// bit
+var hammingWeight = function(n) {
+    let ret =0
+    
+    while(n){        
+        ret++
+        n&=n-1
+    }
+    return ret
+};
