e83160f728ced03fa2c1af8f9dace51e6e681b25
roshad <roshad87a9@gmail.com>
2019-10-18 02:57:12 +0800

    191018

--- 13.roman to integer.js
+++ 13.roman to integer.js
@@ -1,16 +1,18 @@
-map = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000
-};
 var romanToInt = function(s) {
-    let value = 0;
-    for (let i = 0; i < s.length; i++)
-        map[s[i]] < map[s[i + 1]] ? (value -= map[s[i]]) : (value += map[s[i]]);
-
-    return value;
+    let ret = 0;
+    const map = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
+    for (let ind = 0; ind < s.length; ind++) {
+        const cN = map[s[ind]],
+            nN = map[s[ind + 1]];
+        ret += cN < nN ? -cN : cN;
+    }
+    return ret;
 };
--- 3.js
+++ 3.js
@@ -1,12 +1,12 @@
 //1.2
 var lengthOfLongestSubstring = function(s) {
-    const mem={}
-    let max=0    
-    for (indL=0,indR=0;indR<s.length;indR++){
-        const cha = s[indR]
-        indL=Math.max((mem[cha]+1)||0,indL)
-        max=Math.max(max,indR-indL+1)            
-        mem[cha]=indR        
+    const mem = {};
+    let max = 0;
+    for (indL = 0, indR = 0; indR < s.length; indR++) {
+        const cha = s[indR];
+        indL = Math.max(mem[cha] + 1 || 0, indL);
+        max = Math.max(max, indR - indL + 1);
+        mem[cha] = indR;
     }
-    return max
+    return max;
 };
