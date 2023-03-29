b7bc23330997dcafbeed09a756dc2c95f93ddcd1
roshad <roshad87a9@gmail.com>
2023-03-29 16:14:55 +0800

    Update 204. Count Primes.js

--- 204. Count Primes.js
+++ 204. Count Primes.js
@@ -10,3 +10,21 @@ var countPrimes = function(n) {
   }
   return count
 };
+ //如果 x 可以被 primes[j]​整除，那么对于合数 y=x⋅primes[ j+1] ​ 而言，它会在后面遍历到​ x/primes[j] ⋅primes[j+1] ​ 这个数的时候会被标记，其他同理，这保证了每个合数只会被其「最小质因数」筛去一次。
+var countPrimes = function(n) {
+    const isPrime = new Array(n).fill(1);
+    const primes = [];
+   
+    for (let i = 2; i < n; ++i) {
+        if (isPrime[i]) {
+            primes.push(i);
+        }
+        for (let j = 0; j < primes.length && i * primes[j] < n; ++j) {
+            isPrime[i * primes[j]] = 0;
+            if (i % primes[j] === 0) {
+                break;
+            }
+        }
+    }
+    return primes.length;
+};
